7f997f31c8d9a1103d7e53bd1f96e4d055a11e56
Kanad Kumar <kankumar@progress.com>
2022-12-01 15:49:22 +0530

    Omnicaching Test Project
    Signed-off-by: Kanad Kumar <kankumar@progress.com>

---
+++ config/projects/omnicaching.rb
@@ -0,0 +1,27 @@
+#
+# Copyright 2022 YOUR NAME
+#
+# All Rights Reserved.
+#
+
+name "omnicaching"
+maintainer "Progress Software, Inc."
+homepage "https://chef.io"
+
+# Defaults to C:/omnicaching on Windows
+# and /opt/omnicaching on all other platforms
+install_dir "#{default_root}/#{name}"
+# install_dir "/tmp/#{name}"
+
+build_version Omnibus::BuildVersion.semver
+build_iteration 1
+
+# Creates required build directories
+dependency "preparation"
+dependency "omnicaching-zlib"
+
+# omnicaching dependencies/components
+# dependency "somedep"
+
+exclude "**/.git"
+exclude "**/bundler/git"
